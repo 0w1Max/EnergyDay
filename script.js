7a211bb511ea23bdf202a264c1521744ba202f46
Ow1Max <0w1.go.0w1@gmail.com>
2025-12-01 17:10:38 +0300

    feat: implemented the main functionality

---
+++ script.js
@@ -0,0 +1,86 @@
+const energyProgress = document.querySelector('.energy__progress');
+const infoPercent = document.querySelector('.info__percent');
+const energyButton = document.querySelector('.energy__button');
+
+let PROGRESS_DATA = 0;
+const STEP = 5;
+
+const COLORS = {
+  red: 'linear-gradient(90deg, #ff7c7c, #ff4d4d)',
+  orange: 'linear-gradient(90deg, #ffc97c, #ff9900)',
+  green: 'linear-gradient(90deg, #77ff7c, #4caf50)',
+}
+
+function increaseProgress(step) {
+  PROGRESS_DATA = Math.max(0, Math.min(PROGRESS_DATA + step, 100))
+}
+
+function decreaseProgress(step) {
+  PROGRESS_DATA = Math.min(100, Math.max(PROGRESS_DATA - step, 0));
+}
+
+function resetProgress() {
+  PROGRESS_DATA = 0;
+}
+
+function updateProgressColor(view) {
+  let gradient;
+
+  if (PROGRESS_DATA <= 33) {
+    gradient = COLORS.red;
+  } else if (PROGRESS_DATA <= 66) {
+    gradient = COLORS.orange;
+  } else {
+    gradient = COLORS.green;
+  }
+
+  view.style.background = gradient;
+}
+
+function updateButtonState(button) {
+  if (PROGRESS_DATA >= 100) {
+    button.disabled = true
+    button.classList.add("button--disabled")
+  } else {
+    button.disabled = false
+    button.classList.remove("button--disabled")
+  }
+}
+
+function renderProgress(view, text) {
+  view.style.width = `${PROGRESS_DATA}%`;
+  text.textContent = `${PROGRESS_DATA}%`;
+
+  updateProgressColor(view);
+  updateButtonState(energyButton);
+}
+
+function animateDecrease(step = 1, interval = 20) {
+  const timer = setInterval(() => {
+    decreaseProgress(step);
+    renderProgress(energyProgress, infoPercent);
+
+    if (PROGRESS_DATA <= 0) {
+      clearInterval(timer);
+    }
+  }, interval);
+}
+
+function buttonHandler(evt) {
+  evt.preventDefault();
+
+  increaseProgress(STEP);
+  renderProgress(energyProgress, infoPercent);
+
+  if (PROGRESS_DATA === 100) {
+    console.log('Fully charged');
+    
+    setTimeout(function() {
+      animateDecrease()
+  }, 3000);
+  }
+}
+
+renderProgress(energyProgress, infoPercent);
+
+energyButton.addEventListener('click', buttonHandler);
